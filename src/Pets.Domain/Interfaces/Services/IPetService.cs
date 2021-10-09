1acd9b51ebad02b35c5965f4499d3ac1a07d31ee
PauloAlves8039 <pj38alves@gmail.com>
2021-10-09 14:48:03 -0300

    Implementando a regra de negócio

---
+++ src/Pets.Domain/Interfaces/Services/IPetService.cs
@@ -0,0 +1,15 @@
+﻿using Pets.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pets.Domain.Interfaces.Services
+{
+    public interface IPetService
+    {
+        Task<IEnumerable<Pet>> GetAllPets();
+        Task<Pet> GetPetById(int id);
+        Task<Pet> PostPet(Pet pet);
+        Task<Pet> PutPet(Pet pet);
+        Task<bool> DeletePet(int id);
+    }
+}
---
+++ src/Pets.Service/Services/PetService.cs
@@ -0,0 +1,43 @@
+﻿using Pets.Domain.Entities;
+using Pets.Domain.Interfaces;
+using Pets.Domain.Interfaces.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Pets.Service.Services
+{
+    public class PetService : IPetService
+    {
+        private IRepository<Pet> _repository;
+
+        public PetService(IRepository<Pet> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Pet>> GetAllPets()
+        {
+            return await _repository.GetAll();
+        }
+
+        public async Task<Pet> GetPetById(int id)
+        {
+            return await _repository.GetById(id);
+        }
+
+        public async Task<Pet> PostPet(Pet pet)
+        {
+            return await _repository.Insert(pet);
+        }
+
+        public async Task<Pet> PutPet(Pet pet)
+        {
+            return await _repository.Update(pet);
+        }
+
+        public async Task<bool> DeletePet(int id)
+        {
+            return await _repository.Delete(id);
+        }   
+    }
+}
